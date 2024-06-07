fd5915117eb62c696c9eaac2e3878b4ffce6ae3c
KrusteF <krisko100901@gmail.com>
2024-06-07 19:41:23 +0300

    Fix database table namings

--- database/migrations/2024_06_07_154935_create_stacks_table.php
+++ database/migrations/2024_06_07_154935_create_stacks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stack', function (Blueprint $table) {
+        Schema::create('stacks', function (Blueprint $table) {
             $table->id();
             $table->string('value');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('stack');
+        Schema::dropIfExists('stacks');
     }
 };
--- database/migrations/2024_06_07_155051_create_key_value_stores_table.php
+++ database/migrations/2024_06_07_155051_create_key_value_stores_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('key_value_store', function (Blueprint $table) {
+        Schema::create('key_value_stores', function (Blueprint $table) {
             $table->id();
             $table->string('key')->unique();
             $table->text('value');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('key_value_store');
+        Schema::dropIfExists('key_value_stores');
     }
 };
